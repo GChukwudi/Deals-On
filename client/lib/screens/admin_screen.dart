cf5bfe7c30835bd0449caf87e6d4a6c1ed2ecf96
GChukwudi <g.chukwudi@alustudent.com>
2025-07-29 22:26:34 +0200

    updated

--- client/lib/screens/admin_screen.dart
+++ client/lib/screens/admin_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/product_provider.dart';
 import '../models/product.dart';
-import '../widgets/custom_button.dart';
 
 class AdminScreen extends StatefulWidget {
   const AdminScreen({super.key});
